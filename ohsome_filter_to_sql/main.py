16336b736862b101a56663425d1bb8f978420d1a
Matthias Schaub <matthias.schaub@heigit.org>
2025-07-25 11:20:48 +0200

    style: extent ruff ruleset

--- ohsome_filter_to_sql/main.py
+++ ohsome_filter_to_sql/main.py
@@ -1,13 +1,29 @@
+import sys
+
+from antlr4 import CommonTokenStream, InputStream, ParserRuleContext
+
+from ohsome_filter_to_sql.OFLLexer import OFLLexer
 from ohsome_filter_to_sql.OFLListener import OFLListener
+from ohsome_filter_to_sql.OFLParser import OFLParser
 
 
 class OhsomeFilterToSql(OFLListener):
     pass
 
 
-def main():
-    return OhsomeFilterToSql()
+def main(filter: str):
+    build_tree(filter)
+
+
+def build_tree(filter) -> ParserRuleContext:
+    """Build a antlr4 parse tree."""
+    input_stream = InputStream(filter)
+    lexer = OFLLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = OFLParser(stream)
+    tree = parser.root()
+    return tree
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
