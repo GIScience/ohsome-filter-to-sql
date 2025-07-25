cda7d34c72f48ba97265ab0689dd652330671d93
Matthias Schaub <matthias.schaub@heigit.org>
2025-07-25 11:21:43 +0200

    feat: build parser tree

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,17 @@
-from ohsome_filter_to_sql.main import OhsomeFilterToSql, main
+import pytest
+from pytest_approval import verify
+
+from ohsome_filter_to_sql.main import build_tree
 
 
-def test_main():
-    assert isinstance(main(), OhsomeFilterToSql)
+@pytest.mark.parametrize(
+    "filter",
+    (
+        "natural=tree",
+        "type:node and natural=tree",
+        "id:(node/1, way/2) and type:way",
+    ),
+)
+def test_tag(filter):
+    tree = build_tree(filter).toStringTree()
+    assert verify(tree)
