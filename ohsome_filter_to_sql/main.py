d9ba4eb7a104b31cb359c603e66e762d06804787
Matthias Schaub <matthias.schaub@heigit.org>
2025-07-28 11:54:43 +0200

    feat: walk tree and implement dummy listener (wip)

--- ohsome_filter_to_sql/main.py
+++ ohsome_filter_to_sql/main.py
@@ -1,28 +1,50 @@
 import sys
+from collections import deque
 
-from antlr4 import CommonTokenStream, InputStream, ParserRuleContext
+from antlr4 import CommonTokenStream, InputStream, ParserRuleContext, ParseTreeWalker
 
 from ohsome_filter_to_sql.OFLLexer import OFLLexer
 from ohsome_filter_to_sql.OFLListener import OFLListener
 from ohsome_filter_to_sql.OFLParser import OFLParser
 
 
-class OhsomeFilterToSql(OFLListener):
-    pass
+class OFLToSql(OFLListener):
+    def __init__(self):
+        self.stack: deque = deque()
+
+    def exitString(self, ctx):  # noqa: N802
+        self.stack.append(ctx.getChild(0).getText())
+
+    def exitTagMatch(self, ctx):  # noqa: N802
+        value = self.stack.pop()
+        key = self.stack.pop()
+        self.stack.append(key + " = " + value)
 
 
 def main(filter: str):
-    build_tree(filter)
+    listener = OFLToSql()
+    tree = build_tree(filter)
+    translation = walk_tree(tree, listener).stack
+    return " ".join(translation)
 
 
-def build_tree(filter) -> ParserRuleContext:
-    """Build a antlr4 parse tree."""
+def build_tree(filter: str) -> ParserRuleContext:
+    """Build a antlr4 parse tree.
+
+    https://github.com/antlr/antlr4/blob/master/doc/listeners.md
+    """
     input_stream = InputStream(filter)
     lexer = OFLLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = OFLParser(stream)
     tree = parser.root()
     return tree
+
+
+def walk_tree(tree: ParserRuleContext, listener: OFLToSql) -> OFLToSql:
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    return listener
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import pytest
 from pytest_approval import verify
 
-from ohsome_filter_to_sql.main import build_tree
+from ohsome_filter_to_sql.main import build_tree, main
 
 
 @pytest.mark.parametrize(
@@ -15,3 +15,11 @@ from ohsome_filter_to_sql.main import build_tree
 def test_build_tree(filter):
     tree = build_tree(filter).toStringTree()
     assert verify(tree)
+
+
+@pytest.mark.parametrize(
+    "filter",
+    ("natural=tree",),
+)
+def test_main(filter):
+    assert verify(main(filter))
